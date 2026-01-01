04efe4a1c3be6ce8cf8098f8634c7c20c3181967
Majid <hafsa.majid@accenture.com>
2026-01-01 19:12:15 +0000

    Test staging deployment

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Desk Booking App is running!");
+  res.send("Desk Booking App is running");
 });
 
 app.post("/book", (req, res) => {
-  res.json({ message: "Desk booked!" });
+  res.json({ message: "Desk booked" });
 });
 
 app.listen(3000, () => {
