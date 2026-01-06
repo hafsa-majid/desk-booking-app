f179ed121d37a2b2cdffd3cff1bf3b8ac534dd08
Majid <hafsa.majid@accenture.com>
2026-01-06 17:34:16 +0000

    Fix ESLint Jest globals and update config for Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,12 +2,13 @@ module.exports = {
   env: {
     node: true,
     es2022: true,
+    jest: true, // <-- add this line
   },
   extends: 'eslint:recommended',
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
-    // your custom rules here
+    // your custom rules
   },
 };
